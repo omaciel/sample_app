090a49dbcec73bc37123cc4c2d6cf28896d32dc9
Og B. Maciel <ogmaciel@gnome.org>
2011-09-19 12:59:38 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9c6fb1d91c240d7e5790fd38565a4f09eff311a089d39675be0da989fcdd17b690082ed97645629cfef645cb2ea0a86c96eff3e0be2823b815034f06a1edbba1'
